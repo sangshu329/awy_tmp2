7d424a1c005422f9c6b03cbc82e30cf4a609c578
sien <sangshu.sun@163.com>
2015-01-13 18:17:01 +0800

    cp20150113

--- js/about_us.js
+++ js/about_us.js
@@ -13,4 +13,40 @@ $(function(){
     },function(){
         $(this).find(".ddli_month").removeClass("ddli_month_hover");
     })
+
+    var $ab_nav_year=$(".ab_nav_year");
+    var $ab_nav_year_liww=$(".ab_nav_year li:eq(0)").width();
+    var $yearPrev=$(".yearPrev");
+    var $yrefNext=$(".yrefNext");
+//    alert($ab_nav_year_liww);
+    var dir;
+    var num=0;
+    $yearPrev.click(function(){
+        dir=true;
+        if(dir){
+            num=num+1;
+            position=$ab_nav_year_liww*1*num;
+            if(position>($ab_nav_year.children().length-9)*$ab_nav_year_liww){
+                num=num-1;
+                return false;
+            }
+            $ab_nav_year.css({
+                "margin-left":-position
+            })
+        }
+    })
+    $yrefNext.click(function(){
+        dir=false;
+        if(!dir){
+            num=num-1;
+            position=$ab_nav_year_liww*1*num;
+            if(position<0){
+                num=0;
+                return false;
+            }
+            $ab_nav_year.css({
+                "margin-left":-position
+            })
+        }
+    })
 })
